dab07ebe6539ce85a6ae9e4e06f7c3941372a7b6
Alessandro Pigani <94191033+AlessandroPigani@users.noreply.github.com>
2021-11-19 20:03:09 +0000

    Ho leggermente migliorato il programma

--- shampoo.py
+++ shampoo.py
@@ -1,4 +1,4 @@
-values = []
+prezzi = []
 
 my_file = open('shampoo_sales.txt', 'r') 
 #apro il file in modalità lettura (r sta per reading)
@@ -7,16 +7,16 @@ for line in my_file:   #per ogni linea nel mio file:
   #divido gli elementi in corrispondenza della virgola
   #un elemento comicia quando comincia la linea e finisce quando arrivo alla virgola
 
-    if elemento[0] != "Date":  #se l'elemento di indice 1 è diverso da "date":
-        Date = elemento[0]  #l'elemrnto prima della virgola è associato a date
+    if elemento[0] != "Date":  #se l'elemento di indice 0 è diverso da "date":
+        Date = elemento[0]  #l'elemento prima della virgola è associato a date
         valore = elemento[1]  #l'elemento dopo la virgola è associato a values
 
-        values.append(float(valore))  
-        #la funzione append mi aggiunge la variabile valore alla lista values
+        prezzi.append(float(valore))  
+        #la funzione append mi aggiunge la variabile valore alla lista prezzi. Considerando i valori che trova nel file
 
 #prima la lista values era vuota, ora l'ho riempita con i valori (che sono gli elementi di indice 1)
 
-print(values)
+print("i valori sono i seguenti: {}".format(prezzi))
 
 def somma(mia_lista):  
     #sto definendo una funzione somma che ha come parametro una mia lista (a cui associerò values[])
@@ -26,5 +26,5 @@ def somma(mia_lista):
     return totale
 #esco dalla funzione
 
-print(somma(values))  
+print("risultato: {}".format(somma(prezzi)))  
 #richiamo la funzione che ha come parametro la lista values[]
