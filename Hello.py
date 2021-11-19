02e07c6dacce4a57db8bbfc2ebe9d02158bd5c4a
Alessandro Pigani <94191033+AlessandroPigani@users.noreply.github.com>
2021-11-19 13:15:54 +0000

    Ho fatto la somma dei numeri di una lista

--- Hello.py
+++ Hello.py
@@ -1,1 +1,11 @@
 print("Wise men say only fools rush in")
+
+#Scrivete una funzione che sommi tutti gli elementi di una lista.
+
+my_list = [1,3,6,9]  #dichiaro una lista di numeri
+
+somma=0
+
+for item in my_list:  #sono pronto a dare il comando
+  somma = somma + item
+print(somma)
