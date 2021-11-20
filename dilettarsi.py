14893e280ea956a3facd519fe18c6b536f677b0a
Alessandro Pigani <94191033+AlessandroPigani@users.noreply.github.com>
2021-11-20 10:38:13 +0000

    dilettarsi con la funzione append

---
+++ dilettarsi.py
@@ -0,0 +1,17 @@
+my_list = ["Marco", "Giorgia", "Luca"] 
+#i nomi in una lista vanno messi fra virgolette
+print(my_list)
+
+my_list.append("Pietro")
+#applico la fuzione append a my_list
+
+print("la mia nuova lista: {}".format(my_list))
+
+#-----------------------------------------------------------------------
+
+lista_numerica = [1,2,3]
+#i numeri in una lista possono stare senza virgolette
+print(lista_numerica)
+
+lista_numerica.append(4)
+print("la mia nuova lista numerica: {}".format(lista_numerica))
