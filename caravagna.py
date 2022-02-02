26df2a46e7f49911448cfa6909518b02cde525d8
Alessandro Pigani <94191033+AlessandroPigani@users.noreply.github.com>
2022-02-02 14:06:24 +0000

    esercizietti sull'ereditarietà con Caravagna

---
+++ caravagna.py
@@ -0,0 +1,21 @@
+class Studente():
+
+    ruolo = "studente units"   #attributo valido sempre nella classe
+
+    def __init__(self,arg1,arg2): 
+        #self permette all'attributo di sopravvivere una volta uscito dal metodo
+        self.nome = arg1      
+        self.cognome = arg2  
+
+    def bonjour(self):    #non aggiungo altri parametri
+        print("ecco a voi: {}: {} {}". format(self.ruolo, self.nome, self.cognome))
+
+ciapo = Studente("Giulio", "Caravagna")
+
+ciapo.bonjour()
+
+print("campo del nome:", ciapo.nome)
+
+ciapo.nome = "Stefano"  #accedo al campo del nome e lo modifico
+
+print("campo del nome modificato:", ciapo.nome)
