20cdf101c98e525cd7c1e02e4d5097568fe71ea8
Alessandro Pigani <94191033+AlessandroPigani@users.noreply.github.com>
2022-02-07 21:08:28 +0000

    sono riuscito ad ottenere una lista che contiene al suo interno una sottolista  di dati per ogni anno che decido di considerare

--- esame.py
+++ esame.py
@@ -38,19 +38,41 @@ def compute_avg_monthly_difference(listone, first_year, last_year):
     #una lista per anno
     for i,item in enumerate(listone): #per ogni listino in time_series
         
-       
+        
         lista = []
        
                    #start         #stop    #step
         for i in range(0, len(time_series), 12):
             lista.append(listone[i : i+12])
 
-        #ok sono riuscito a creare liste da 12 elementi
-        #ma tutto è ancora all'interno del listone
-        
-    return lista
+    print(lista)   #all'interno del listone ho creato delle liste, ogni lista interna al listone contiene i dati di un anno
 
-print(compute_avg_monthly_difference(time_series, "1949", "1960"))
+    listini = []  #ci inserirò le liste degli anni che mi interessano
+    
+
+    first_year = float(first_year) - 1949
+    last_year = float(last_year) - 1949
+    
+
+    
+    for i,item in enumerate(lista):  #itero sulla lista
+        if(i>=first_year and i<=last_year):
+            listini.append(lista[i])
+
+    print("_________________________________________________________")
+
+    print(listini)
+
+    
+
+    
+    
+
+    #sono riuscito a creare una lista per i dati del 1950, una per i dati del 1951, una per i dati del 1952
+        
+    
+
+compute_avg_monthly_difference(time_series, "1950", "1952")
 
 
     
